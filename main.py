5a099d2b1bdc70e217d27b87812896763e0221ad
ARyback <adam.ryback@gmail.com>
2022-02-02 19:16:57 -0600

    I figured out how to make a random choice for each option

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world!')
+print('Welcome to destination day trip generator! You came to the right place!')
+
+import random
+
+destination_list = ['Milwaukee', 'Chicago', 'Wausau', 'Green Bay', 'Minneapolis']
+restaurant_list = ['Calderone Club', 'County Clare', 'Crafty Cow', 'BBQ City' , 'Cada de Alberto']
+mode_of_transport = ['Car', 'Plane', 'Train', 'Truck', 'Motorcycle']
+entertainment_list = ['Jimmy Buffett Concert', 'Football Game', 'Museum Tour', 'Comedy Show', 'Brewery Tour']
+
+rand_destination = destination_list[random.randint(0, 4)]
+rand_restaurant = restaurant_list[random.randint(0, 4)]
+rand_transportation = mode_of_transport[random.randint(0, 4)]
+rand_entertainment = entertainment_list[random.randint(0, 4)]
+
