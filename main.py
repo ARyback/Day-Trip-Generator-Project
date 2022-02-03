0cd63f193ca40baa439972b15f4f3571b74918a6
ARyback <adam.ryback@gmail.com>
2022-02-02 19:35:14 -0600

    I figured out how to do randomnly select a destination per the instructions.

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ restaurant_list = ['Calderone Club', 'County Clare', 'Crafty Cow', 'BBQ City' , 
 mode_of_transport = ['Car', 'Plane', 'Train', 'Truck', 'Motorcycle']
 entertainment_list = ['Jimmy Buffett Concert', 'Football Game', 'Museum Tour', 'Comedy Show', 'Brewery Tour']
 
-rand_destination = destination_list[random.randint(0, 4)]
+#rand_destination = destination_list[random.randint(0, 4)]
 rand_restaurant = restaurant_list[random.randint(0, 4)]
 rand_transportation = mode_of_transport[random.randint(0, 4)]
 rand_entertainment = entertainment_list[random.randint(0, 4)]
+user_dest = 'y'
 
+while user_dest == 'y':
+    rand_destination = destination_list[random.randint(0, 4)]
+    destination_option = input(f'We have selected {rand_destination} as your city of choice! Do you like it? Enter y or n: ')
+    if destination_option == 'y':
+        print(f'Congratulations on choosing {rand_destination}! Let us move on')
+    else:
+        rand_destination = destination_list[random.randint(0, 4)]
+        destination_option = input(f'We have selected {rand_destination} as your city of choice! How does this sound? Enter y or n: ')
+        if user_dest == 'y':
+            print(f'Congratulations on choosing {rand_destination}! Let us move on.')
+            break
