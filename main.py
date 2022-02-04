bcace0c5517c0c415763a6f952177f38d1d95d5b
ARyback <adam.ryback@gmail.com>
2022-02-04 14:49:01 -0600

    Fixed first problem you mentioned. However, I duplicated another, which will need to be solved later.

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ restaurant_list = ['Calderone Club', 'County Clare', 'Crafty Cow', 'BBQ City' , 
 transportation_list = ['Car', 'Plane', 'Train', 'Truck', 'Motorcycle']
 entertainment_list = ['Jimmy Buffett Concert', 'Football Game', 'Museum Tour', 'Comedy Show', 'Brewery Tour']
 
-def welcome():
-    print('Welcome to the day trip generator! You came to the right place!')
+total_list = [[destination_list], [restaurant_list], [transportation_list], [entertainment_list]]
+
+choose_initial = input(f"Do you like the initial trip we picked for you? y or n: ")
+
+def initial_option(option_list):
+    rand_option = option_list[random.randint(0, 4)]
+    return rand_option
+
+def initial_trip():
+    initial_destination = initial_option(destination_list)
+    initial_restaurant = initial_option(restaurant_list)
+    initial_transportation = initial_option(transportation_list)
+    initial_entertainment = initial_option(entertainment_list)
+    print(f"Congratulations on choosing {initial_destination} as your destination, {initial_restaurant} as your restaurant, {initial_transportation} as your transportation, and {initial_entertainment} as your entertainment.")  
+
+
 
 def option_generator(option_list, activity):
     while True:
@@ -17,15 +31,15 @@ def option_generator(option_list, activity):
             return rand_option
             break
 
-    
-
 def day_trip_generator():
-    welcome()
-    destination_generator = option_generator(destination_list, "destination")
-    restaurant_generator = option_generator(restaurant_list, "restaurant")
-    transportation_generator = option_generator(transportation_list, "mode of transportation")
-    entertainment_generator = option_generator(entertainment_list, "entertainment")
-    print(f"Congratulations on choosing {destination_generator} as your destination, {restaurant_generator} as your restaurant, {transportation_generator} as your transportation, and {entertainment_generator} as your entertainment.")
+    initial_trip()
+    choose_initial
+    if choose_initial == "n":
+        destination_generator = option_generator(destination_list, "destination")
+        restaurant_generator = option_generator(restaurant_list, "restaurant")
+        transportation_generator = option_generator(transportation_list, "mode of transportation")
+        entertainment_generator = option_generator(entertainment_list, "entertainment")
+        print(f"Congratulations on choosing {destination_generator} as your destination, {restaurant_generator} as your restaurant, {transportation_generator} as your transportation, and {entertainment_generator} as your entertainment.")
     
 day_trip_generator()
 
